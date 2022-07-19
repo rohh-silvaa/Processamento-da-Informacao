e780ed0688e0a67d66ac2d16183366eb2186e71c
Rohh_Silvaa <109565616+rohh-silvaa@users.noreply.github.com>
2022-07-18 23:22:23 -0300

    Delete EP1_1Celsius2Fahrenheit.java

--- EP1_1Celsius2Fahrenheit.java
+++
@@ -1,7 +0,0 @@
-public class Programa {
-  public static void main(String[] args) {
-    int C = 65;
-    float F = C * 9/5 + 32;
-    System.out.println(C+" graus Celsius corresponde a "+F+" graus Fahrenheit");
-  }
-}
