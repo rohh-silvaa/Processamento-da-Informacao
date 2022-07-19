145ed72ee3d9353c80683a8131b0a51fc09d947a
Rohh_Silvaa <109565616+rohh-silvaa@users.noreply.github.com>
2022-07-18 23:21:11 -0300

    Create EP1_1Celsius2Fahrenheit.java

---
+++ EP1_1Celsius2Fahrenheit.java
@@ -0,0 +1,7 @@
+public class Programa {
+  public static void main(String[] args) {
+    int C = 65;
+    float F = C * 9/5 + 32;
+    System.out.println(C+" graus Celsius corresponde a "+F+" graus Fahrenheit");
+  }
+}
